944feb736caef298528ee9dfa0374278cc4eb6ae
ranggakamajaya <kamajaya.rangga@gmail.com>
2023-04-28 14:34:07 +0700

    first changes

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -7,19 +7,13 @@ export const About = () => {
         <span className='text-lg lg:text-xl font-bold'>Selamat datang di Industrial Mercenary!</span><br />
         Kami adalah perusahaan yang berfokus pada membantu mahasiswa dalam dunia perkuliahan. Kami menyediakan berbagai layanan untuk membantu mahasiswa meraih kesuksesan akademik dan karir mereka.
         <br /><br />
-        Sebagai perusahaan yang berpengalaman, kami memahami bahwa setiap mahasiswa memiliki kebutuhan dan tantangan yang berbeda. Oleh karena itu, kami menawarkan berbagai layanan yang dapat disesuaikan dengan kebutuhan masing-masing mahasiswa. Beberapa layanan yang kami tawarkan antara lain:
-        <br /><br />
-        <ul className='list-disc list-inside'>
-            <li>Jasa penulisan makalah dan tugas akademik</li>
-            <li>Bimbingan skripsi</li>
-            <li>Konsultasi karir dan Pengembangan soft skills</li>
-            <li>Penyewaan dan tugas tambahan</li>
-        </ul>
+        Sebagai perusahaan yang berpengalaman, kami memahami bahwa setiap mahasiswa memiliki kebutuhan dan tantangan yang berbeda. Oleh karena itu, kami menawarkan berbagai layanan yang dapat disesuaikan dengan kebutuhan masing-masing mahasiswa. 
         <br />
         Kami memiliki tim yang terdiri dari ahli di bidang akademik dan karir, yang siap membantu mahasiswa mencapai tujuan mereka. Selain itu, kami selalu mengutamakan kualitas dan kepercayaan dalam setiap layanan yang kami tawarkan.
         Kami percaya bahwa pendidikan adalah kunci untuk meraih kesuksesan di masa depan. Oleh karena itu, kami berkomitmen untuk memberikan layanan terbaik kepada setiap mahasiswa yang membutuhkan bantuan dalam dunia perkuliahan.
         <br /><br />
-        Terima kasih telah memilih Industrial Mercenary sebagai mitra dalam perjalanan akademik dan karir Anda. Kami siap membantu Anda meraih kesuksesan yang Anda inginkan.</p>
+        Terima kasih telah memilih Industrial Mercenary sebagai mitra dalam perjalanan akademik dan karir Anda. Kami siap membantu Anda meraih kesuksesan yang Anda inginkan.
+        </p>
     </div>
   )
 }
