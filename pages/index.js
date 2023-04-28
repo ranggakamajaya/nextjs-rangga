3575e9ae984406662d3b0df282e803ed60374979
ranggakamajaya <kamajaya.rangga@gmail.com>
2023-04-28 15:02:13 +0700

    improvement

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Hero from '../components/index/Hero'
 import HeadlineCard from '@/components/index/HeadlineCard'
-import { Main } from 'next/document'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
